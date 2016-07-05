440d749c77a26e80c3240e9b2a393dea3d4282cb
Darius Kurtinaitis <dariuskrtn@gmail.com>
2016-07-05 23:23:55 +0300

    pam param

--- Assets/Spectrum.cs
+++ Assets/Spectrum.cs
@@ -6,9 +6,11 @@ public class Spectrum : MonoBehaviour {
 	public GameObject prefab;
 	public int numberOfObjects = 20;
 	public float radius = 5f;
-
+	public AudioClip audi;
 	GameObject[] cubes;
-
+	public int Samples = 2048;
+	public int kuris = 3;
+	public float size = 1;
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < numberOfObjects; i++) {
@@ -21,11 +23,27 @@ public class Spectrum : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float[] spectrum = AudioListener.GetSpectrumData (1024, 0, FFTWindow.Hamming);
+		float[] spectrum = AudioListener.GetSpectrumData (Samples, 0, FFTWindow.Hamming);
 		for (int i = 0; i < numberOfObjects; i++) {
 			Vector3 previousScale = cubes [i].transform.localScale;
 			previousScale.y = spectrum [i] * 20;
 			cubes [i].transform.localScale = previousScale;
+			if (previousScale.y > size && i==kuris) {
+				bum ();
+				Debug.Log (i + "");
+			}
 		}
+	}
+	void bum () {
+		if (GameObject.Find ("naujas")!=null)
+			Object.Destroy (GameObject.Find ("naujas"));
+		Color color = new Color (Random.value, Random.value, Random.value);
+		MeshRenderer mesh = prefab.GetComponent<MeshRenderer> ();
+
+		Material mat = new Material (Shader.Find("Standard"));
+		mat.color = color;
+		mesh.material = mat;
+		Object midd = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+		midd.name = "naujas";
 	}
 }
