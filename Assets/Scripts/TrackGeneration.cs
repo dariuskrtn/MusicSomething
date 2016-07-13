ed97eb4e7ead628a10bb790880f323703bd9bf91
Darius Kurtinaitis <dariuskrtn@gmail.com>
2016-07-13 21:54:43 +0300

    Some generation updates

--- Assets/Scripts/TrackGeneration.cs
+++ Assets/Scripts/TrackGeneration.cs
@@ -8,14 +8,21 @@ public class TrackGeneration : MonoBehaviour {
     public GameObject kick;
     public GameObject snare;
 
+	public int maxJumpsInARow = 5;
+	public float speedDistBeforeObstacle = 5f;
 	public float maxSpeed = 50f;
 	public float minSpeed = 15f;
 
     public float moveSpeed = 0.1f;
 
-	private GameObject lastObstacle;
-	private GameObject lastGround;
+	private float currHeight = -5f;
+
+	private GameObject lastSpike;
+	private GameObject lastBlackground;
+	public GameObject lastGround;
 	private GameObject lastJump;
+	private int jumpStreak = 0;
+
 	private float lastTime = 0f;
 	private float lastSpeed = 25f;
 
@@ -31,6 +38,7 @@ public class TrackGeneration : MonoBehaviour {
 		source = GetComponent<AudioSource> ();
         GetComponent<BeatDetection>().CallBackFunction = onBeat;
 		StartCoroutine(StartRunning());
+		StartCoroutine (checkGround());
 		myRigidbody = GetComponent<Rigidbody2D> ();
     }
 	void Update() {
@@ -44,15 +52,16 @@ public class TrackGeneration : MonoBehaviour {
 		player.updateVelocity (moveSpeed);
 	}
 
+
     public void onBeat(BeatDetection.EventInfo eventInfo)
     {
 		if (moveSpeed<maxSpeed)
 			moveSpeed += (maxSpeed-moveSpeed)/10f;
-		generateJumpSphere (Jump);
         switch (eventInfo.messageInfo)
         {
 		case BeatDetection.EventType.HitHat:
 			generateLine (hithat);
+			generateJumpSphere (Jump);
                 break;
         case BeatDetection.EventType.Kick:
             generateLine(kick);
@@ -70,7 +79,7 @@ public class TrackGeneration : MonoBehaviour {
 		Vector3 pos = new Vector3(transform.position.x, getPlayerYPrediction(), 0);
 		GameObject obj = Instantiate(sphere, pos, Quaternion.identity) as GameObject;
 		GameObject.Destroy(obj, 10);
-		lastObstacle = obj;
+		lastJump = obj;
 		lastSpeed = player.jumpForce;
 		lastTime = Time.time;
 	}
@@ -78,7 +87,7 @@ public class TrackGeneration : MonoBehaviour {
 	{
         // Neatsiras prie pat starto
 		if (transform.position.x < 0) return;
-		Vector3 pos = new Vector3(transform.position.x + (moveSpeed/5), -3.94f, 0);
+		Vector3 pos = new Vector3(transform.position.x + (moveSpeed/speedDistBeforeObstacle), -3.94f, 0);
 		GameObject obj = Instantiate(type, pos, Quaternion.identity) as GameObject;
 		GameObject.Destroy(obj, 10);
 	}
@@ -95,11 +104,44 @@ public class TrackGeneration : MonoBehaviour {
 		GetComponent<AudioSource> ().Play ();
 		running = true;
 	}
+
+	IEnumerator checkGround() {
+		if (lastJump == null && lastBlackground == null) {
+			expandGround ();
+		} else {
+			float obstacle = 0f;
+			if (lastJump != null)
+				obstacle = lastJump.transform.position.x;
+			if (lastBlackground != null) {
+				obstacle = Mathf.Max (obstacle, lastBlackground.transform.position.x - lastBlackground.transform.localScale.x / 2f);
+			}
+			if (obstacle < lastGround.transform.position.x) {
+				expandGround ();
+			} else {
+				createGround ();
+			}
+		}
+		yield return new WaitForSeconds(2f);
+		StartCoroutine (checkGround());
+	}
+	private void createGround ()
+	{
+		Vector3 pos = new Vector3 (transform.position.x, getPlayerYPrediction()-1f, 0f);
+		GameObject obj = Instantiate (Ground, pos, Quaternion.identity) as GameObject;
+		lastGround = obj;
+		lastSpeed = moveSpeed;
+	}
 	private float getPlayerYPrediction() {
 		float totalTime = Time.time - lastTime;
-		float lastPos = lastObstacle==null?-4:lastObstacle.transform.position.y;
+		float lastPos = lastJump==null?-4:lastJump.transform.position.y;
 		Rigidbody2D pl = player.GetComponent<Rigidbody2D> ();
 		float prePos = lastPos + lastSpeed * totalTime + (pl.gravityScale*pl.mass*Physics2D.gravity.y*totalTime*totalTime) / 2;
 		return Mathf.Max(prePos, -4f);
+	}
+	private void expandGround()
+	{
+			float howMuch = transform.position.x - lastGround.transform.position.x + lastGround.transform.localScale.x / 2f;
+		lastGround.transform.localScale = new Vector3 (lastGround.transform.localScale.x + howMuch, 1, 1);
+		lastGround.transform.position = new Vector2 (lastGround.transform.position.x + howMuch / 2f, lastGround.transform.position.y);
 	}
 }
